12b6b22c222b10509ecde3f39051cb6fd8ce0825
garamalho <bielramalho02@gmail.com>
2019-08-15 11:33:23 -0300

    first commit

---
+++ src/boraRamalho/Livro.java
@@ -0,0 +1,21 @@
+
+package boraRamalho;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private String ispn;
+    private int num_de_paginas;
+    private int ano;
+    private String editora;
+    private double valor;
+    
+    public void setTitulo (String titulo){
+        return this.titulo;
+    }
+    public String getTitulo (String titulo)(
+            
+    return this.titulo=titulo (String titulo)
+            this.titulo=titulo;
+    )
+}
---
+++ src/boraRamalho/Principal.java
@@ -0,0 +1,10 @@
+
+package boraRamalho;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
